4b7f1bd5fa8d7cdc5b36326690f3fd0f9a8d29df
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-02-17 06:30:27 +0100

    début optimisation site

--- 0_initialisation/get2.php
+++
@@ -1,5 +0,0 @@
-<h1><?php 
-
-echo "Hello From ". $_GET['nom']." et j'ai ". $_GET['age']."ans.";
-
-?></h1>
--- 0_initialisation/post2.php
+++
@@ -1,5 +0,0 @@
-<h1><?php 
-
-echo "Hello From post2, je suis ". $_POST['nom']." et j'ai ". $_POST['age']."ans.";
-
-?></h1>
